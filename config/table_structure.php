9813314da45cf01aa5533eaa20aea752ae371b4b
Vicente Russo <vicente.russo@gmail.com>
2021-04-19 23:25:50 -0300

    remove not used config

--- config/table_structure.php
+++ config/table_structure.php
@@ -12,23 +12,4 @@ return [
     */
     'use_cache' => false,
 
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | Database Connections
-    |--------------------------------------------------------------------------
-    |
-    | Here are each of the database connections setup for your application.
-    | Of course, examples of configuring each database platform that is
-    | supported by Laravel is shown below to make development simple.
-    |
-    |
-    | All database work in Laravel is done through the PHP PDO facilities
-    | so make sure you have the driver for your particular database of
-    | choice installed on your machine before you begin development.
-    |
-    */
-    'cache_connection' => env('TABLE_STRUCTURE_CONNECTION', 'redis'),
-
 ];
--- src/TableStructure.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace VRusso\TableStructure;
-
-class TableStructure
-{
-}
